76b7da8f6eb5922ab8c85d185f9819d1aee407d5
Shubham Prashar <67927586+oyeprashar@users.noreply.github.com>
2024-09-18 10:59:01 +0530

    Update Find All Duplicates in an Array (0 to n-1).py

--- Arrays/Find All Duplicates in an Array (0 to n-1).py
+++ Arrays/Find All Duplicates in an Array (0 to n-1).py
@@ -1,15 +1,18 @@
 class Solution:
     def duplicates(self, arr, n): 
-    	
-    	ans = []
-    	
-    	for i in range(len(arr)):
-    	    num = arr[i] % n 
-    	    
-    	    arr[num] += n
-    	   
-    	    
+        
+        ans = []
+        
         for i in range(len(arr)):
+            num = arr[i] % n 
+            
+            # treat this num as index and mark visited there since range is (0, N-1) so all elements are there as index also
+            arr[num] += n
+           
+            
+        for i in range(len(arr)):
+
+            # if an index was marked more than once, the index (element) was the repeating number (again logic due to range of num)
             if arr[i] // n > 1:
                 ans.append(i)
         
@@ -17,3 +20,9 @@ class Solution:
             return [-1]
         
         return sorted(ans)
+
+
+s = Solution()
+arr = [0, 3, 1, 3]
+
+print(s.duplicates(arr, len(arr)))
