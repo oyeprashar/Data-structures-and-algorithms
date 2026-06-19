96131294a3eec84626152a160c0b286f7429554b
Shubham Prashar <67927586+oyeprashar@users.noreply.github.com>
2026-06-19 10:57:29 +0530

    Update Bellmanford SSSP.py

--- Graphs/Bellmanford SSSP.py
+++ Graphs/Bellmanford SSSP.py
@@ -1,59 +1,37 @@
 """
-Bellman-ford algo
+Bellman Ford (handles -ve edges)
+
+Unlike Dijkstra, we DONT keep visited  and relax all the edges for V times
 """
-from collections import defaultdict 
+
 INT_MAX = 3**38
 
-class Graph:
-	def __init__(self,V):
-		self.V = V
-		self.vertList = []
-	
-	def addEdge(self,u,v,cost):
-		self.vertList.append((u,v,cost))
-	
-	def bellmanford(self):
+def bellman_ford(edges, number_of_nodes, src):
 
-		dist = [INT_MAX] * self.V 
-		dist[0] = 0
+    distance = [INT_MAX] * number_of_nodes
+    distance[src] = 0
 
-		for _ in range(self.V):
 
-			for list1 in self.vertList:
-				u = list1[0]
-				v = list1[1]
-				cost = list1[2]
+    for _ in range(number_of_nodes):
+        for edge in edges:
+            u = edge[0]
+            v = edge[1]
+            cost = edge[2]
 
-				if dist[u] + cost < dist[v]:
-					dist[v] = dist[u] + cost 
+            if distance[u] != INT_MAX:
+                distance[v] = min( distance[v], distance[u] + cost)
 
-		# checking for the negative cycles
-		for list1 in self.vertList:
-				u = list1[0]
-				v = list1[1]
-				cost = list1[2]
 
-				if dist[u] + cost < dist[v]:
-					return "NEGATIVE CYCLE FOUND"
-						
-		
-				
-		return dist 
-	
-g = Graph(5)
-g.addEdge(0, 1, -1)
-g.addEdge(0, 2, 4)
-g.addEdge(1, 2, 3)
-g.addEdge(1, 3, 2)
-g.addEdge(1, 4, 2)
-g.addEdge(3, 2, 5)
-g.addEdge(3, 1, 1)
-g.addEdge(4, 3, -3)
-print(g.bellmanford())
+    # another loop to detect negative cycle
+    for edge in edges:
+        u = edge[0]
+        v = edge[1]
+        cost = edge[2]
 
-g1 = Graph(4)
-g1.addEdge(0,1,1)
-g1.addEdge(1,2,-1)
-g1.addEdge(2,3,-1)
-g1.addEdge(3,0,-1)
-print(g1.bellmanford())
+        if distance[u] + cost < distance[v]:
+            return "negative cycle found"
+
+
+    return distance
+
+
