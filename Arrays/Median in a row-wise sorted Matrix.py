eaf15fb12e5b3feb19876fd150cea4808e7a23d0
Shubham Prashar <67927586+oyeprashar@users.noreply.github.com>
2026-06-22 09:34:59 +0530

    Update Median in a row-wise sorted Matrix.py

--- Arrays/Median in a row-wise sorted Matrix.py
+++ Arrays/Median in a row-wise sorted Matrix.py
@@ -1,52 +1,55 @@
+import heapq
+
+
+class Item:
+    def __init__(self, value, rowIndex, colIndex):
+        self.value = value
+        self.rowIndex = rowIndex
+        self.colIndex = colIndex
+
+    def __lt__(self, other):
+        return self.value <= other.value
+
 class Solution:
-    def countNum(self,left,right,rowIndex,matrix,key,ans):
+    
+    def find_median(self, mat, target_index):
+        min_heap = []
+        for rowIndex in range(len(mat)):
+            item = Item(mat[rowIndex][0], rowIndex, 0)
+            heapq.heappush(min_heap, item)
 
-        if left <= right:
-    
-            mid = (left+right) // 2
-    
-            if matrix[rowIndex][mid] <= key:
-                ans[0] = mid+1
-                return self.countNum(mid+1,right,rowIndex,matrix,key,ans)
-            
-            else:
-                return self.countNum(left,mid-1,rowIndex,matrix,key,ans)
+        count = 0
+        while len(min_heap) > 0 and count != target_index:
+
+            top = heapq.heappop(min_heap)
+            count += 1
+
+            if count == target_index:
+                return top.value
+
+            if top.colIndex + 1 < len(mat[top.rowIndex]):
+                new_value = mat[top.rowIndex][top.colIndex + 1]
+                new_item = Item(new_value, top.rowIndex, top.colIndex + 1)
+                heapq.heappush(min_heap, new_item)
                 
-                
-    def findMedian(self,left,right,mat,reqCount):
+        return -1
 
-        if left <= right:
-    
-            mid = (left+right) // 2
-    
-            totalCount = 0
-            
-    
-            for row in range(len(mat)):
-                cnt = [0]
-                self.countNum(0,len(mat[row])-1,row,mat,mid,cnt)
-                totalCount += cnt[0]
-    
-            if totalCount < reqCount:
-                return self.findMedian(mid+1,right,mat,reqCount)
-            
-            else:
-                return self.findMedian(left,mid-1,mat,reqCount)
-
-        else:
-            return left
-
-    def median(self, matrix, r, c):
-    	left = 0
-        right = 10**9
-        totalCount = 0
-        
-        for row in range(len(matrix)):
-            totalCount += len(matrix[row])
-        
-        reqCount = (totalCount//2) + 1
-        
-        return self.findMedian(left,right,matrix,reqCount)
+    def median(self, mat):
+        target_index = ((len(mat) * len(mat[0])) // 2) + 1
+        return self.find_median(mat, target_index)
 
 
-        
+mat1 = [[1, 3, 5],
+        [2, 6, 9],
+        [3, 6, 9]]
+
+mat2 = [[2, 4, 9],
+        [3, 6, 7],
+        [4, 7, 10]]
+
+mat3 = [[3], [4], [8]]
+
+s = Solution()
+print(s.median(mat1))
+print(s.median(mat2))
+print(s.median(mat3))
