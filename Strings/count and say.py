6338f1303acbfe396089d9c0ce6e7fd3b164c7ea
Shubham Prashar <67927586+oyeprashar@users.noreply.github.com>
2026-06-24 09:47:20 +0530

    Update count and say.py

--- Strings/count and say.py
+++ Strings/count and say.py
@@ -1,28 +1,59 @@
+"""
+Input: n = 4
+
+Output: "1211"
+
+Explanation:
+
+countAndSay(1) = "1"
+countAndSay(2) = RLE of "1" = "11"
+countAndSay(3) = RLE of "11" = "21"
+countAndSay(4) = RLE of "21" = "1211"
+"""
+    
 class Solution:
-    def countAndSay(self, n: int) -> str:
-        
-        if n == 1:
-            return "1"
-        
-        prev = self.countAndSay(n-1)
-        
-        currChar = prev[0]
+
+    # Apply the rules to a single string
+    # frequency + char
+    def RLE(self, string):
+
+        # lets say the number is 21
+        prev = string[0]
         count = 1
-        curr = ""
-        
-        for i in range(1,len(prev)):
-            
-            if prev[i] == currChar:
+        res = ""
+        currIndex = 1
+
+        while currIndex <= len(string):
+
+            if currIndex == len(string):
+                res += str(count)
+                res += prev
+                return res
+
+            if string[currIndex] == prev:
                 count += 1
-            
-            elif prev[i] != currChar:
-                curr += str(count)
-                curr += currChar
-                
-                currChar = prev[i]
+
+            elif string[currIndex] != prev:
+                res += str(count)
+                res += prev
                 count = 1
-            
-        curr += str(count)
-        curr += currChar
-        
-        return curr 
+                prev = string[currIndex]
+
+            currIndex += 1
+
+        return res
+
+    def countAndSay(self, n):
+
+        if n == "1":
+            return 1
+
+        string = "1"
+        for _ in range(n - 1):
+            string = self.RLE(string)
+
+        return string
+
+
+s = Solution()
+print(s.countAndSay(4)) # 1211
