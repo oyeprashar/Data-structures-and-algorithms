087792b04bca222a2abbb004bfd2d4b74bc91398
Shubham Prashar <67927586+oyeprashar@users.noreply.github.com>
2026-06-26 11:00:08 +0530

    Update agressiveCows.py

--- Binary Search/agressiveCows.py
+++ Binary Search/agressiveCows.py
@@ -1,39 +1,56 @@
-def isValid(minDist,numCows,stalls):
+"""
+In this question we have to maximise the min valid distance
+"""
 
-    curr = stalls[0]
-    placed = 1
-    for i in range(1,len(stalls)):
-        if stalls[i] - curr >= minDist:
-            placed += 1
-            curr = stalls[i]
+class Solution:
 
-    return placed >= numCows
+    
+    def isValid(self, minDistanceLimit, stalls, numOfCows):
 
-def searchAns(left,right,numCows,stalls,ans):
-    if left <= right:
+        # since the array is sorted, placing the first cow on the zeroth index gives the largest room for 
+        # other cowss
+        lastCowPostion = 0
+        placedCows = 1
 
-        mid = (left+right) // 2
+        for i in range(1, len(stalls)):
 
-        if isValid(mid,numCows,stalls):
-            # save it and maximize it by moving to the right
-            ans[0] = mid
-            return searchAns(mid+1,right,numCows,stalls,ans)
-        else:
-            # move to the left to find a valid ans
-            return searchAns(left,mid-1,numCows,stalls,ans)
+            # place a cow if current min distance obeyed
+            if stalls[i] - stalls[lastCowPostion] >= minDistanceLimit:
+                placedCows += 1
+                lastCowPostion = i
 
-def findDist(numCows,stalls):
-    ans = [-1]
-    stalls.sort()
-    minRange = 0; maxRange = stalls[-1]-stalls[0]
-    searchAns(minRange,maxRange,numCows,stalls,ans)
-    return ans[0]
+        return placedCows >= numOfCows
+
+
+    def binarySearchOnAnswer(self, left, right, stalls, numOfCows, res):
+
+        if left <= right:
+
+            mid = (left + right) // 2
+
+            # if the distance is valid, save it and maximise it
+            if self.isValid(mid, stalls, numOfCows):
+                res[0] = max(res[0], mid) # save the answer
+                # move right to maximise this distance
+                return self.binarySearchOnAnswer(mid + 1, right, stalls, numOfCows, res)
+
+            else:
+                return self.binarySearchOnAnswer(left, mid - 1, stalls, numOfCows, res)
+
+
+    def placeCows(self, stalls, numOfCows):
+        stalls.sort()
+        res = [-3**38]
+        maximumDist = stalls[-1] - stalls[0]
+        self.binarySearchOnAnswer(0, maximumDist, stalls, numOfCows, res)
+        return res[0]
 
 test_cases = int(input())
+s = Solution()
 for _ in range(test_cases):
     n,numCows = map(int,input().split())
     stalls = []
     for i in range(n):
         x = int(input())
         stalls.append(x)
-    print(findDist(numCows,stalls))
+    print(s.placeCows(stalls, numCows))
