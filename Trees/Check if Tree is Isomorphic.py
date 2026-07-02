32700d958055c365fedb8ce0f1b2df431d040202
Shubham Prashar <67927586+oyeprashar@users.noreply.github.com>
2026-07-02 11:01:17 +0530

    Update Check if Tree is Isomorphic.py

--- Trees/Check if Tree is Isomorphic.py
+++ Trees/Check if Tree is Isomorphic.py
@@ -1,16 +1,26 @@
+"""
+In isomorphic we have two options
+    1. FLip root1's left with root2's right and root1's right with root2's left
+                                or
+    2. Dont flipt and compare root1's left with root2's left and root1's right with root2's right
+"""
 class Solution:
-    # Return True if the given trees are isomotphic. Else return False.
+    
     def isIsomorphic(self, root1, root2): 
-        if root1 == None and root2 == None:
-            return True
-            
-        if root1 == None or root2 == None:
-            return False
         
+        # we were able to traverse both trees successfully
+        if root1 is None and root2 is None:
+            return True
+
+        # if one of them was None
+        if root1 is None or root2 is None:
+            return False
+
+        # data mismatch
         if root1.data != root2.data:
             return False
+            
+        flipRes = self.isIsomorphic(root1.left, root2.right) and self.isIsomorphic(root1.right, root2.left) 
+        noFlipRes =  self.isIsomorphic(root1.left, root2.left) and self.isIsomorphic(root1.right, root2.right)
         
-        op1 = self.isIsomorphic(root1.left,root2.left) and self.isIsomorphic(root1.right,root2.right)
-        op2 = self.isIsomorphic(root1.left,root2.right) and self.isIsomorphic(root1.right,root2.left)
-        
-        return op1 | op2
+        return flipRes or  noFlipRes
