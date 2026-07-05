ae91c7f6f30f4e4609372bba23bd166f908cbb3a
Shubham Prashar <67927586+oyeprashar@users.noreply.github.com>
2026-07-05 10:47:38 +0530

    Update Print all k-sum paths in a binary tree.py

--- Trees/Print all k-sum paths in a binary tree.py
+++ Trees/Print all k-sum paths in a binary tree.py
@@ -1,51 +1,44 @@
 """
-A binary tree and a number k are given. 
-Print every path in the tree with sum of the nodes in the path as k. 
-A path can start from any node and end at any node and must be downward only,
-i.e. they need not be root node and leaf node; and negative numbers can also be there in the tree.
+- Print every path in the tree with sum of the nodes in the path as k.
+- A path can start from any node and end at any node and must be downward only
 """
+
 class Node:
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
 
+def pathWithSumKHelper(root, k, currPath):
 
-def findPaths(root,currPath,ans,k):
+    if root is None:
+        return None
 
-    if root == None:
-        return 
-
+    # Add the node to the current path and check moving upwards
+    # This makes sure all the paths are considered moving upwards
     currPath.append(root.data)
-    findPaths(root.left,currPath,ans,k)
-    findPaths(root.right,currPath,ans,k)
-    
-    curr = []
     currSum = 0
-    index = len(currPath)-1
+    currNodes = []
+    for i in range(len(currPath)-1,-1,-1):
 
-    while index >= 0:
-
-        curr.append(currPath[index])
-        currSum += currPath[index]
+        currNodes.append(currPath[i])
+        currSum += currPath[i]
 
         if currSum == k:
-            ans.append(curr[::-1])
+            print(currNodes[::-1]) # since we want to add the nodes downwards but the loop was down to up
 
-        index -= 1
+    # go left and right
+    pathWithSumKHelper(root.left, k, currPath)
+    pathWithSumKHelper(root.right, k, currPath)
 
+    # remove the node from the current path
     currPath.pop()
 
-def getPaths(root,k):
 
-    ans = []
+def pathWithSumK(root, k):
+    pathWithSumKHelper(root, k, [])
 
-    findPaths(root,[],ans,k)
-
-    return ans
-
-   
-
+# Test code
 root = Node(1)
 root.left = Node(3)
 root.left.left = Node(2)
@@ -56,6 +49,5 @@ root.right.left = Node(4)
 root.right.left.left = Node(1)
 root.right.left.right = Node(2)
 root.right.right = Node(5)
-root.right.right.right = Node(6)
-
-print(getPaths(root,5))
+root.right.right.right = Node(2)
+pathWithSumK(root, 5)
