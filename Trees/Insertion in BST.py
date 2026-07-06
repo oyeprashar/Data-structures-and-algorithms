17e2122a3a6ec600e7eaf24634f9dc5573bffb98
Shubham Prashar <67927586+oyeprashar@users.noreply.github.com>
2026-07-06 09:53:26 +0530

    Update Insertion in BST.py

--- Trees/Insertion in BST.py
+++ Trees/Insertion in BST.py
@@ -1,43 +1,18 @@
 class Node:
-
-    def __init__(self,data):
+    def __init__(self, data):
         self.data = data
-        self.left = None 
-        self.right = None 
+        self.left = None
+        self.right = None
 
-def insert(root,key):
+def insertIntoBST(self, root, newData):
+    
+        if root is None:
+            return Node(newData)
 
-    if root == None:
-        return Node(key)
-
-    elif key < root.data:
-
-        if root.left == None:
-            root.left = Node(key)
-
+        if root.data > newData:
+            root.left = self.insertIntoBST(root.left, newData)
         else:
-            insert(root.left,key)
+            root.right = self.insertIntoBST(root.right, newData)
 
-    elif key > root.data:
-
-        if root.right == None:
-            root.right = Node(key)
-
-        else:
-            insert(root.right,key)
-
-
-def inorder(root):
-    if root:
-        inorder(root.left)
-        print(root.data)
-        inorder(root.right)
-
-r = Node(50)
-insert(r, 30)  # we are saying r = insert() because insert function is returning the root of the new BST
-insert(r, 20)
-insert(r, 40)
-insert(r, 70)
-insert(r, 60)
-insert(r, 80)
-inorder(r)
+        return root
+    
