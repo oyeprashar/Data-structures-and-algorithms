0923cfdfc7a55cf2d9cad224840ed2325aa0ea31
Shubham Prashar <67927586+oyeprashar@users.noreply.github.com>
2026-07-07 09:47:30 +0530

    Update largest BST in a BT.py

--- Trees/largest BST in a BT.py
+++ Trees/largest BST in a BT.py
@@ -1,39 +1,53 @@
+"""
+The reason why we return INT_MIN as maxValue and INT_Max as minValue when we encounter None is because when the call gets
+returned to the leaf node and we check `root.data > leftMaxValue and root.data < rightMinValue` we want it to pass!
+
+
+
+"""
+
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.left = None
+        self.right = None
+
 INT_MAX = 3**38
 INT_MIN = -3**38
 
 class Solution:
-    
-    def largestBstHelper(self,root,max1,min1):
-        
-        if root == None:
-            return 1,0,INT_MIN,INT_MAX
-        
-        if root.right == None and root.left == None:
-            return 1,1,root.data,root.data
-        
-        isLeftBST,leftSize,leftMax,leftMin = self.largestBstHelper(root.left,max1,min1)
-        isRightBST,rightSize,rightMax,rightMin = self.largestBstHelper(root.right,max1,min1)
-        
-        
-        if isLeftBST == 1 and isRightBST == 1 and root.data > leftMax and root.data < rightMin:
-            if leftMin == INT_MAX:
-                leftMin = root.data
-            
-            if rightMax == INT_MIN:
-                rightMax = root.data
-            
-            
-            return 1,leftSize+1+rightSize,rightMax,leftMin
-        
+
+    def largestBSTHelper(self, root, minValue, maxValue):
+
+        if root is None:
+            return True, 0, INT_MAX, INT_MIN # because the min and max is used to validate the leaf nodes
+
+        isLeftBST, leftSize, leftMinValue, leftMaxValue = self.largestBSTHelper(root.left, minValue, maxValue)
+        isRightBST, rightSize, rightMinValue, rightMaxValue = self.largestBSTHelper(root.right, minValue, maxValue)
+
+        if isLeftBST is True and isRightBST is True and root.data > leftMaxValue and root.data < rightMinValue:
+
+            if leftMinValue == INT_MAX:
+                leftMinValue = root.data
+
+            if rightMaxValue == INT_MIN:
+                rightMaxValue = root.data
+
+            return True, leftSize + 1 + rightSize, leftMinValue, rightMaxValue
+
         else:
-            return 0,max(leftSize,rightSize),0,0
-            
-    
-    
+            return False, max(leftSize, rightSize), -1, -1
+
     def largestBst(self, root):
-        if root == None:
-            return 1
-        
-        isBST,size1,max1,min1 = self.largestBstHelper(root,INT_MAX,INT_MIN)
-        
-        return size1
+        _, maxBSTSize, _, _ = self.largestBSTHelper(root, minValue=INT_MAX, maxValue=INT_MIN)
+        return maxBSTSize
+
+# Test code
+root = Node(6)
+root.left = Node(7)
+root.left.right = Node(2)
+root.right = Node(3)
+root.right.left = Node(2)
+root.right.right = Node(4)
+s = Solution()
+print(s.largestBst(root))
