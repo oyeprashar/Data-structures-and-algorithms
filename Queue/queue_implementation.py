1e054b88f051542391bdd1ee669575e016e858e4
Shubham Prashar <67927586+oyeprashar@users.noreply.github.com>
2026-07-13 09:33:31 +0530

    Update queue_implementation.py

--- Queue/queue_implementation.py
+++ Queue/queue_implementation.py
@@ -1,24 +1,65 @@
-class Queue():
-    # defining the attributes
+"""
+Implement queue such that insertion and deletion both are O(1) operations
+"""
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class Queue:
     def __init__(self):
-        self.items = []
-    def enqueue(self,item):
-        self.items.insert(0,item)
-    def dequeue(self):
-        if not(self.isEmpty()):
-            return self.items.pop()
+        self.head = None
+        self.tail = None
+
+    # O(1)
+    def insert(self, data):
+        newNode = Node(data)
+       
+        if self.tail is None:
+            self.head = newNode
+            self.tail = newNode
         else:
-            return "UNDERFLOW"
-    def isEmpty(self):
-        return self.items == []
-    def size(self):
-        return len(self.items)
-q = Queue()
-q.enqueue(1)
-q.enqueue(2)
-q.enqueue(3)
-print(q.dequeue())
-print(q.dequeue())
-print(q.dequeue())
-print(q.dequeue())
-print(q.size())
+            self.tail.next = newNode
+            self.tail = newNode
+
+     # O(1)
+    def delete(self):
+
+        if self.head is None:
+            return
+
+        temp = self.head
+        self.head = self.head.next
+        
+        # no items for tail to point at
+        if self.head is None:
+            self.tail = None
+        
+        return temp.data
+
+    def __str__ (self):
+
+        ans = ""
+        curr = self.head
+        while curr:
+            ans += str(curr.data)
+            ans += " "
+            curr = curr.next
+        return ans
+
+queue = Queue()
+queue.insert(1)
+queue.insert(2)
+queue.insert(3)
+queue.insert(4)
+queue.delete()
+queue.delete()
+print(queue)
+
+# print(queue)
+# queue.delete()
+# print(queue)
+
+
