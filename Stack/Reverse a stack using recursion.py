17187b03c6e085a41cfb695f6c604c729c5d24e8
Shubham Prashar <67927586+oyeprashar@users.noreply.github.com>
2026-07-13 11:00:43 +0530

    Update Reverse a stack using recursion.py

--- Stack/Reverse a stack using recursion.py
+++ Stack/Reverse a stack using recursion.py
@@ -1,12 +1,47 @@
+"""
+    Approach :
+        - Recursively remove elements from the stack until there is no more element
+        - Now insert the element st index zero goes to the end of the stack
+"""
 
-def reverse(stack):
-    if len(stack) == 0:
-        return None 
-    
-    currPop = stack.pop(0)
-    reverse(stack)
-    print(currPop)
+class Solution:
 
-stack = [1,2,3,4]
-reverse(stack)
-    
+    # Now insert the element st index zero goes to the end of the stack
+    def insertAtStart(self, stack, num):
+
+        # We want to insert the element at the bottom
+
+        # if no elements are there in the stack then append the element
+        if len(stack) == 0:
+            stack.append(num)
+            return
+
+        # remove the elements
+        top  = stack.pop()
+        self.insertAtStart(stack, num)
+
+        # add the element back in the order they were
+        stack.append(top)
+
+
+    # Recursively remove elements from the stack until there is no more element
+    def reverseStackHelper(self, stack):
+
+        if len(stack) == 0:
+            return
+
+        top = stack.pop()
+        self.reverseStackHelper(stack)
+
+        # now we have the first element, and we want to insert it at the bottom of the stack
+        self.insertAtStart(stack, top)
+
+        # insert it at the last of the stack
+
+
+    def reverseStack(self, stack):
+        self.reverseStackHelper(stack)
+        return stack
+
+s = Solution()
+print(s.reverseStack([1, 2, 3, 4, 5, 6]))
