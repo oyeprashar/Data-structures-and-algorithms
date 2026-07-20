142c1cea3366b1164c23dd2aed9fe3e2cda34984
Shubham Prashar <sirprashar@gmail.com>
2026-07-20 10:18:33 +0530

    fixed the bug in prims

--- Graphs/Prims to find MST (undirected).py
+++ Graphs/Prims to find MST (undirected).py
@@ -1,16 +1,3 @@
-"""
-**** Implement prims ****
-
-Algo
-	1. use a min heap (vertex, cost)
-	2. While we have not selected all the vertices
-		i. pop the top of min heap
-	   ii. if not in visited, add to visited, add the cost
-	  iii. Loop over the nbrs, and add them to heap if they are not visited
-
-
-"""
-
 import heapq
 from collections import defaultdict
 
@@ -36,23 +23,20 @@ class Solution:
             adj[u].append([v, edgeCost])
             adj[v].append([u, edgeCost])
 
-        print("adj :",adj)
-
         minHeap = []
         heapq.heappush(minHeap, Item(0, 0))
         totalCost = 0
         visited = set()
 
-        while len(minHeap) > 0 and V != 0:
+        while len(minHeap) > 0 and len(visited) < V:
 
             top = heapq.heappop(minHeap)
+            
+            if top.vertex in visited:
+                continue
 
             totalCost += top.cost
             visited.add(top.vertex)
-            V -= 1
-
-            if V == 0:
-                return totalCost
 
             for nbr in adj[top.vertex]:
 
@@ -60,6 +44,3 @@ class Solution:
                     heapq.heappush(minHeap, Item(nbr[0], nbr[1]))
 
         return totalCost
-
-s = Solution()
-print(s.spanningTree(V = 3, edges = [[0, 1, 5], [1, 2, 3], [0, 2, 1]]))
