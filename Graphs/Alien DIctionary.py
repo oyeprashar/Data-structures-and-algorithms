1b5f5125b3a18e4e44a3eba4a6bae5da4e54463a
Shubham Prashar <sirprashar@gmail.com>
2026-07-23 11:04:39 +0530

    updated alien dictionary

--- Graphs/Alien DIctionary.py
+++ Graphs/Alien DIctionary.py
@@ -1,47 +1,91 @@
-#User function Template for python3
-
 from collections import defaultdict
 
 class Solution:
-    
-    def topologicalSort(self,currV,adj,visited,ordering):
-        
+
+    def cycleDetectionDirected(self, currV, cycleVisited, currPath, adj):
+
+        cycleVisited.add(currV)
+        currPath.add(currV)
+
+        for nbr in adj[currV]:
+
+            if nbr not in cycleVisited:
+                if self.cycleDetectionDirected(nbr, cycleVisited, currPath, adj):
+                    return True
+            else:
+                if nbr in currPath:
+                    return True
+
+        currPath.remove(currV)
+        return False
+
+    def generateGraph(self, words):
+
+        adj = defaultdict(list)
+
+        for i in range(len(words) -1):
+
+            word1 = words[i]
+            word2 = words[i + 1]
+            diffFound = False
+
+            for i in range(min(len(word1), len(word2))):
+                if word1[i] != word2[i]:
+                    adj[word1[i]].append(word2[i])
+                    diffFound = True
+                    break
+
+            # is there is no char diff between word1 and word2 and word1 is longer, then it is an invalid ordering
+            if diffFound is False and len(word1) > len(word2):
+                return None, False
+
+        return adj, True
+
+    def dfs(self, currV, visited, ordering, adj):
+
         visited.add(currV)
-        
+
         for nbr in adj[currV]:
             if nbr not in visited:
-                self.topologicalSort(nbr,adj,visited,ordering)
-            
+                self.dfs(nbr, visited, ordering, adj)
+
         ordering.append(currV)
-                
-    def findOrder(self, dictionary, N, K):
-        
-        adj = defaultdict(list)
-        
-        for i in range(len(dictionary)-1):
-            
-            word1 = dictionary[i]
-            word2 = dictionary[i+1]
-            
-            size = min(len(word1),len(word2))
-            
-            for j in range(size):
-                
-                if word1[j] != word2[j]:
-                    adj[word1[j]].append(word2[j])
-                    
-                    break
-      
-        visited = set()
+
+    def findOrder(self, words):
+
+        """
+            1. Generate the graph and check the validity of the ordering
+            2. Cycle check in the directed graph
+            3. Return its topological sorted order
+        """
+
+        adj, isValid = self.generateGraph(words)
+
+        if not isValid:
+            return ""
+
         ordering = []
-        
-        
-        for x in range(K):
-            char = chr(ord('a') + x)
-            
-            if char not in  visited:
-                self.topologicalSort(char,adj,visited,ordering)
-                
-        
+        visited = set()
+
+        uniqueChars = set()
+        for word in words:
+            for char in list(word):
+                uniqueChars.add(char)
+
+        currPath = set()
+        cycleVisited = set()
+
+        for currV in uniqueChars:
+            if currV not in cycleVisited:
+                if self.cycleDetectionDirected(currV, cycleVisited, currPath, adj):
+                    return ""
+
+        # keys = adj.keys()
+        for currV in uniqueChars:
+            if currV not in visited:
+                self.dfs(currV, visited, ordering, adj)
+
         return ordering[::-1]
 
+s = Solution()
+print(s.findOrder(["ab", "cd", "ef", "ad"]))
