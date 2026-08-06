d50df60506621e0ed5945e47ad6fb6f3be2e9b56
Shubham Prashar <sirprashar@gmail.com>
2026-08-06 17:21:24 -0400

    updated sort012

--- Arrays/segregate 0s and 1s and 2s (Dutch Flag Algo).py
+++ Arrays/segregate 0s and 1s and 2s (Dutch Flag Algo).py
@@ -1,20 +1,28 @@
 class Solution:
-    def sort012(self,arr,n):
-        low = 0 
-        mid = 0
-        high = len(arr)-1
-        
+    def sort012(self, arr):
+        low = 0  # for 0
+        mid = 0  # for 1
+        high = len(arr) - 1 # for 2
+
+
         while mid <= high:
-    
+
+            """
+            This is the most important part. Since low and mid were initialised with the same index,
+            elements till low - 1 are all zeroes
+            """
             if arr[mid] == 0:
-                arr[mid],arr[low] = arr[low],arr[mid]
+                arr[mid], arr[low] = arr[low], arr[mid]
                 mid += 1
                 low += 1
-            
+
             elif arr[mid] == 1:
                 mid += 1
-            
+
+
             elif arr[mid] == 2:
-                arr[mid],arr[high] = arr[high],arr[mid]
+                arr[mid], arr[high] = arr[high], arr[mid]
                 high -= 1
-            
+
+        return arr
+
