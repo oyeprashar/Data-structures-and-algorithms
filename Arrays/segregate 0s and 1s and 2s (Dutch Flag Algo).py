96b1bb8c14db6d0e5fbdd22f2564dbfbc2386899
Shubham Prashar <sirprashar@gmail.com>
2026-08-07 09:48:19 -0400

    added comment to understand dutch flag invariant

--- Arrays/segregate 0s and 1s and 2s (Dutch Flag Algo).py
+++ Arrays/segregate 0s and 1s and 2s (Dutch Flag Algo).py
@@ -1,3 +1,15 @@
+"""
+[ 0s ][ 1s ][ unknown ][ 2s ]
+        ↑       ↑         ↑
+       low     mid       high
+
+
+[0, low - 1] -> all 0s
+[low to mid - 1] -> all 1s
+[mid ... high] → We don't know what they are, and we process them
+[high + 1, end] -> all 2s
+"""
+
 class Solution:
     def sort012(self, arr):
         low = 0  # for 0
@@ -7,10 +19,6 @@ class Solution:
 
         while mid <= high:
 
-            """
-            This is the most important part. Since low and mid were initialised with the same index,
-            elements till low - 1 are all zeroes
-            """
             if arr[mid] == 0:
                 arr[mid], arr[low] = arr[low], arr[mid]
                 mid += 1
@@ -20,9 +28,10 @@ class Solution:
                 mid += 1
 
 
+            # Here when we are swapping mid with high, we are not sure what new element is sitting at mid
+            # So we just move the high pointer and keep the mid where it was to process the new element
             elif arr[mid] == 2:
                 arr[mid], arr[high] = arr[high], arr[mid]
                 high -= 1
 
         return arr
-
