4af2d2387162eff52868d09b8b9c15bc0d8188aa
Shubham Prashar <sirprashar@gmail.com>
2026-08-11 11:28:23 -0400

    updated buy sell stock - ii

--- Arrays/Best Time to Buy and Sell Stock II (sell any number of stocks).py
+++ Arrays/Best Time to Buy and Sell Stock II (sell any number of stocks).py
@@ -1,11 +1,16 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        
+    def maxProfit(self, prices):
+
+        """
+            - A stock can be bought once and told once, but we can do unlimited transactions.
+            - Simply do as many profitable transactions as possible!
+
+        """
+
         totalProfit = 0
-        
-        for i in range(1,len(prices)):
-            
-            if prices[i] > prices[i-1]:
-                totalProfit += prices[i] - prices[i-1]
-            
+        for i in range(1, len(prices)):
+
+            if prices[i] > prices[i - 1]:
+                totalProfit += prices[i] - prices[i - 1]
+
         return totalProfit
