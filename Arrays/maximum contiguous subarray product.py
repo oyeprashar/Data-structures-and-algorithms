87ffd73bf7ffdf5b256fab8606ad1b3a9af1c14f
Shubham Prashar <sirprashar@gmail.com>
2026-08-11 10:53:44 -0400

    updated Maximum Product Subarray

--- Arrays/maximum contiguous subarray product.py
+++ Arrays/maximum contiguous subarray product.py
@@ -1,17 +1,28 @@
 class Solution:
+
+    # Function to find maximum
+    # product subarray
     def maxProduct(self, arr):
-        maxP = arr[0]
-        minP = arr[0]
-        ans = arr[0]
-        
-        for i in range(1,len(arr)):
-            
+
+        """
+        A neg number makes the largest number smallest and the smallest number largest so we need to keep track of
+        both max and min sum and when a neg number comes we swap them!
+        """
+
+        maxProduct = arr[0]
+        minProduct = arr[0]
+        res = arr[0]
+
+        for i in range(1, len(arr)):
+
             if arr[i] < 0:
-                maxP,minP = minP,maxP
-                
-            maxP = max(arr[i],arr[i]*maxP)
-            minP = min(arr[i],arr[i]*minP)
-            
-            ans = max(ans,maxP)
-        
-        return ans
+                maxProduct, minProduct = minProduct, maxProduct
+
+            maxProduct = max(maxProduct * arr[i], arr[i])
+            minProduct = min(minProduct * arr[i], arr[i])
+
+            res = max(res, maxProduct)
+
+        return res
+
+
