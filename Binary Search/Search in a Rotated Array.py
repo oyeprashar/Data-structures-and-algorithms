0eb9875f2f221a57aea66771e785939fd17455a6
Shubham Prashar <sirprashar@gmail.com>
2026-08-26 10:45:38 -0400

    updated search in rotated sorted array

--- Binary Search/Search in a Rotated Array.py
+++ Binary Search/Search in a Rotated Array.py
@@ -1,76 +1,66 @@
 class Solution:
-    
-    def binarySearch(self,left,right,arr,key):
-        
-        if left <= right:
-            
-            mid = (left+right) // 2
-            
-            if arr[mid] == key:
-                return mid
-            
-            if key > arr[mid]:
-                return self.binarySearch(mid+1,right,arr,key)
-            
-            else:
-                return self.binarySearch(left,mid-1,arr,key) 
-        
-        else:
+
+    # Binary search to find out the pivot index
+    """
+    Properties of a pivot index (depending upon where the pivot index is) :
+        1. arr[i] > arr[i + i]
+        2. arr[i] < arr[i-1]
+    """
+    def getPivotIndex(self, left, right, arr):
+
+        if left > right:
             return -1
-    
-    def pivot(self,left,right,arr):
-        
-        if left <= right:
 
-            mid = (left+right) // 2
+        mid = (left + right) // 2
 
-            # this is the logic to identify the pivot element
-            if mid == 0 and arr[mid] > arr[mid+1]:
-                return mid + 1
+        # conditions to check pivot index
+        if mid != len(arr) - 1 and arr[mid] > arr[mid + 1]:
+            return mid
 
-            if mid == len(arr)-1 and arr[mid] < arr[mid-1]:
-                return mid
+        elif mid != 0 and arr[mid] < arr[mid - 1]:
+            return mid - 1
 
-            elif arr[mid] < arr[mid-1]:
-                return mid
-            
-            elif mid != len(arr)-1:
-                if arr[mid] > arr[mid+1]:
-                    return mid+1
+        # if none of the cases were hit, we change the search space
+        if arr[left] < arr[mid]: # left subarray is uniform, move to right
+            return self.getPivotIndex(mid + 1, right, arr)
+        else:
+            # move left
+            return self.getPivotIndex(left, mid - 1, arr)
 
-            # this is the logic to move to search space
+    def binarySeach(self, left, right, arr, target):
 
-            if arr[mid] > arr[left]:
-                # left side is uniform so we move to the right side
-                return self.pivot(mid+1,right,arr)
+        if left > right:
+            return -1
 
-            else:
-                return self.pivot(left,mid-1,arr)
+        mid = (left + right) // 2
 
-    def search(self, arr : list, l : int, h : int, key : int):
-        
-        
-        if len(arr) == 1:
-            if arr[0] == key:
-                return 0
-            else:
-                return -1
-        
-        if len(arr) == 2:
-            if arr[0] == key:
-                return 0
-            if arr[1] == key:
-                return 1
-            else:
-                return -1
-        
-        p = self.pivot(0,len(arr)-1,arr)
-        # print(p,arr[p])
-        if p == None:
-            return self.binarySearch(0,len(arr)-1,arr,key)
+        if arr[mid] == target:
+            return mid
 
-        leftAns = self.binarySearch(0,p-1,arr,key)
-        rightAns = self.binarySearch(p,len(arr)-1,arr,key)
+        if arr[mid] > target:
+            return self. binarySeach(left, mid - 1, arr, target)
+
+        else:
+            return self. binarySeach(mid + 1, right, arr, target)
+
+
+    def search(self, arr, target):
+        pivotIndex = self.getPivotIndex(0, len(arr) - 1, arr)
+
+        """
+        Important : We have to search in both of the arrays!
+        Lets take the following example arr = [5, 6, 7, 8, 9, 10, 1, 2, 3], target = 3. Here the pivot element is 10
+        and if we say target is less than pivot and search just in the left sub-array, we will never find it!
+        """
+
+        if pivotIndex == -1:
+            return self.binarySeach(0, len(arr) - 1, arr, target)
+
+        if arr[pivotIndex] == target:
+            return pivotIndex
+
+        leftAns = self.binarySeach(0, pivotIndex - 1, arr, target)
+        rightAns = self.binarySeach(pivotIndex + 1, len(arr) - 1, arr, target)
 
         if leftAns == -1 and rightAns == -1:
             return -1
@@ -80,5 +70,7 @@ class Solution:
 
         if rightAns != -1:
             return rightAns
-        
-        return -1
+
+
+s = Solution()
+print(s.search(arr = [5, 6, 7, 8, 9, 10, 1, 2, 3], target = 10))
