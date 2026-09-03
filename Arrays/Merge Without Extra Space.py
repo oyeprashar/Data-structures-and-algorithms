6f700be31489479e32daa423e14d9e0f9cb216e5
Shubham Prashar <sirprashar@gmail.com>
2026-09-03 10:34:35 -0400

    updated merge two sorted array in place

--- Arrays/Merge Without Extra Space.py
+++ Arrays/Merge Without Extra Space.py
@@ -1,26 +1,26 @@
+
+"""
+- The approach is to keep all the smaller elements in arr1 and bigger elements in the arr2
+- Since both arrays are sorted, we do not have to process all the elements
+- We simply put a pointer on the biggest element of arr1 and smallest element of arr2 and while elements in arr1 are
+    bigger than elements in arr2, we keep on swapping them
+- After this loop exists, all the bigger elements are in arr2
+- Sort arr1 and arr2
+"""
+
+
 class Solution:
-    def merge(self, arr1, arr2, n, m): 
-        
+    def mergeArrays(self, arr1, arr2):
+
+        i = len(arr1) - 1 # biggest element of arr1
+        j = 0 # smallest element of arr2
+
+        while i >= 0 and j < len(arr2) and arr1[i] > arr2[j]:
+            arr1[i], arr2[j] = arr2[j], arr1[i]
+            i -= 1
+            j += 1
+
         arr1.sort()
         arr2.sort()
 
-        
-        i = len(arr1)-1
-        j = 0
-        
-        # The largest of arr1 should be smaller than the smallest of arr! Swap while they aint
-        while i >=0 and j < len(arr2) and arr1[i] > arr2[j]:
-            
-            arr1[i],arr2[j] = arr2[j],arr1[i]
-            i -= 1
-            j += 1
-        
-        arr1.sort()
-        arr2.sort()
-        
-        # nlogn + mlogm
-        
-        
-        # O(nlogn + mlogm + m + n)
-        
-        return arr1,arr2
+        return arr1, arr2
