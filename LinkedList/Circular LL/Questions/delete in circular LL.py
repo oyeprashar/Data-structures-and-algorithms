9239bc08782bf531b051b639ef63aeed019e58fe
Shubham Prashar <sirprashar@gmail.com>
2026-09-10 12:29:51 -0400

    updated deletion in a circular ll

--- LinkedList/Circular LL/Questions/delete in circular LL.py
+++ LinkedList/Circular LL/Questions/delete in circular LL.py
@@ -1,48 +1,50 @@
-'''
 class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
 
-'''
-
+"""
+This is a circular linkedlist where the last node is connected with the head
+"""
 
 class Solution:
-    
-    def deleteNode(self, head, key):
-    
-        if head is None:
-            return head
 
-        # deleting the head
-        if head.data == key:
-
-            # head is the only node in the linkedin list
-            if head.next == head:
-                return None
-
-            # make the last node point to head.next and move head to head.next
-            oldHead = head
-            head = head.next
-
-            curr = head
-            while curr.next != oldHead:
-                curr = curr.next
-
-            curr.next = head
-            return head
+    def getNodeBeforeTarget(self, head, target):
 
         curr = head
 
-        # find the node before the target node and make it point to point next to the target
-        while True:
-
-            if curr.next.data == key:
-                curr.next = curr.next.next
-                return head
-
+        # [MOST Important] : since this is a circular linked list. There will be no none limiting it!
+        while curr.next is not head:
+            if curr.next.data == target:
+                return curr
             curr = curr.next
 
-            # we were not able to find the target node and again came back to the head 
-            if curr == head:
-                return head
+        return None
+
+
+    def deleteNode(self, head, key):
+
+        # case 1 : head
+        if head.data == key:
+
+            curr = head
+            while curr.next is not head:
+                curr = curr.next
+
+            curr.next = head.next
+            head = head.next
+            return head
+
+        nodeBeforeTarget = self.getNodeBeforeTarget(head, key)
+
+        if nodeBeforeTarget is None:
+            return head
+
+        # case 2 : last node
+        if nodeBeforeTarget.next.next == head:
+            nodeBeforeTarget.next = head
+            return head
+
+        # case 3 : some generic node between head and last
+        nodeBeforeTarget.next = nodeBeforeTarget.next.next
+        return head
