4dc31b4fb52396a61b60bcae86c70637f11a5f0e
Shubham Prashar <sirprashar@gmail.com>
2026-09-16 11:30:15 -0400

    updated code for finding diameter

--- Trees/Diameter of Tree.py
+++ Trees/Diameter of Tree.py
@@ -1,25 +1,29 @@
+"""
+Important update :
+    In GFG, earlier they wanted us to return the number of nodes but now they want then number of edges.
+
+    Counting the edges in the diameter :
+        - Our height code is still counting the number of nodes
+        - When we check the diameter, we wont do +1 to it and this will make sure we are counting the edges and not nodes
+"""
+
+
+
+
+
 class Solution:
-    
-    def height(self,root,ans):
-        
-        if root == None:
+
+    def getDiameter(self, root, res):
+        if root is None:
             return 0
-            
-        lHeight = self.height(root.left,ans)
-        rHeight = self.height(root.right,ans)
-        
-        ans[0] = max(ans[0],lHeight + 1 + rHeight)
-        
-        return 1 + max(lHeight,rHeight)
-    
-    def diameter(self,root):
-        
-        if root == None:
-            return 0
-        
-        ans = [-3**38]
-        
-        self.height(root,ans)
-        
-        return ans[0]
-        
+
+        leftHeight = self.getDiameter(root.left, res)
+        rightHeight = self.getDiameter(root.right, res)
+        res[0] = max(res[0], leftHeight + rightHeight)
+
+        return 1 + max(leftHeight, rightHeight)
+
+    def diameter(self, root):
+        res = [-3 ** 38]
+        self.getDiameter(root, res)
+        return res[0]
