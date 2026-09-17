18fcfea847cac2e622f5b79388ddbb3c555abe1a
Shubham Prashar <sirprashar@gmail.com>
2026-09-17 11:46:59 -0400

    updated boundary traversal code

--- Trees/Boundary Traversal of binary tree.py
+++ Trees/Boundary Traversal of binary tree.py
@@ -1,61 +1,84 @@
+"""
+A very important note : <------ EXTREMELY IMPORTANT
+    In this problem, the left side answer is not left view. Because in left view, it is literally how the tree would look
+    if we saw it from the left
+
+    The left answer is the left boundary, a continuous left path.
+
+    The same logic applies to the right path
+"""
+
+
+
+
 class Solution:
-    def leftSide(self,root,leftArr):
-        
-        if root == None or root.left == None and root.right == None:
-            return 
-        
-    
-        leftArr.append(root.data)
-        
-        if root.left != None:
-            self.leftSide(root.left,leftArr)
-        
-        elif root.right != None:
-            self.leftSide(root.right,leftArr)
-            
-    def rightSide(self,root,rightArr):
-        
-        if root == None or root.left == None and root.right == None:
-            return 
-        
-        rightArr.append(root.data)
-        
-        if root.right != None:
-            self.rightSide(root.right,rightArr)
-            
-        elif root.left != None:
-            self.rightSide(root.left,rightArr)
-            
-    def leafNodes(self,root,leafArr):
-        
-        if root == None:
-            return 
-        
-        if root.left == None and root.right == None:
-            leafArr.append(root.data)
-        
-        self.leafNodes(root.left,leafArr)
-        self.leafNodes(root.right,leafArr)
-            
-    
-    def printBoundaryView(self, root):
-        if root == None:
+
+    def getLeftBoundary(self, root, currLevel, lastLevel, leftBoundary):
+
+        if root is None or (root.left is None and root.right is None):
+            return
+
+        if currLevel > lastLevel[0]:
+            leftBoundary.append(root.data)
+            lastLevel[0] = currLevel
+
+        # We do this because we want to trace the left boundary
+        if root.left is not None:
+            self.getLeftBoundary(root.left, currLevel + 1, lastLevel, leftBoundary)
+        else:
+            self.getLeftBoundary(root.right, currLevel + 1, lastLevel, leftBoundary)
+
+    def getRightBoundary(self, root, currLevel, lastLevel, rightBoundary):
+
+        if root is None or (root.left is None and root.right is None):
+            return
+
+        if currLevel > lastLevel[0]:
+            rightBoundary.append(root.data)
+            lastLevel[0] = currLevel
+
+        # We do this because we want to trace the right boundary
+        if root.right is not None:
+            self.getRightBoundary(root.right, currLevel + 1, lastLevel, rightBoundary)
+
+        else:
+            self.getRightBoundary(root.left, currLevel + 1, lastLevel, rightBoundary)
+
+    def getLeaves(self, root, leaves):
+
+        if root is None:
+            return
+
+        if root.left is None and root.right is None:
+            leaves.append(root.data)
+            return
+
+        self.getLeaves(root.left, leaves)
+        self.getLeaves(root.right, leaves)
+
+    def boundaryTraversal(self, root):
+
+        if root is None:
             return []
-            
-        leftArr = []
-        self.leftSide(root.left,leftArr)
-        
-        rightArr = []
-        self.rightSide(root.right,rightArr)
-        
-        leafArr = []
-        self.leafNodes(root,leafArr)
-        
-        ans = [root.data]
-        
-        ans.extend(leftArr)
-        ans.extend(leafArr)
-        ans.extend(rightArr[::-1])
-        
-        return ans
-            
+
+        if root.left is None and root.right is None:
+            return [root.data]
+
+        leftBoundary = []
+        rightBoundary = []
+        leaves = []
+
+        self.getLeftBoundary(root.left, 0, [-1], leftBoundary)
+        self.getRightBoundary(root.right, 0, [-1], rightBoundary)
+        self.getLeaves(root, leaves)
+
+        # rightBoundary.pop(0)
+
+        res = []
+        res.append(root.data)
+        res.extend(leftBoundary)
+
+        res.extend(leaves)
+        res.extend(rightBoundary[::-1])
+
+        return res
