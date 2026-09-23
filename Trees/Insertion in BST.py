3cdb26c9bb149a4bf551dc73facf62d2fc64e79d
Shubham Prashar <sirprashar@gmail.com>
2026-09-23 11:02:20 -0400

    updated code for insertion in BST

--- Trees/Insertion in BST.py
+++ Trees/Insertion in BST.py
@@ -1,18 +1,29 @@
+"""
+    - The most important thing is that e are not trying to create a balanced BST
+    - We simply iterate the binary search tree till we find the leaf where we can insert the new node either in the
+        left or right
+"""
+
+
+
 class Node:
+
     def __init__(self, data):
         self.data = data
         self.left = None
         self.right = None
 
-def insertIntoBST(self, root, newData):
-    
-        if root is None:
-            return Node(newData)
+class Solution:
+    # Function to insert a node in a BST.
+    def insert(self, root, key):
 
-        if root.data > newData:
-            root.left = self.insertIntoBST(root.left, newData)
-        else:
-            root.right = self.insertIntoBST(root.right, newData)
+        if root is None:
+            return Node(key)
+
+        if key < root.data:
+            root.left = self.insert(root.left, key)
+
+        elif key > root.data:
+            root.right = self.insert(root.right, key)
 
         return root
-    
