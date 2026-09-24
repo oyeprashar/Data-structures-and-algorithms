fed0318338310191324188ed16452fbe4910c748
Shubham Prashar <sirprashar@gmail.com>
2026-09-24 10:54:01 -0400

    updated code to balance a BST

--- Trees/Convert a normal BST to Balanced BST.py
+++ Trees/Convert a normal BST to Balanced BST.py
@@ -1,61 +1,35 @@
+
 class Node:
-    def __init__(self,data):
-        self.data = data
-        self.left = None
+    def __init__(self, val):
         self.right = None
+        self.data = val
+        self.left = None
 
-def getInorder(root,arr):
 
-    if root:
-        getInorder(root.left,arr)
+
+class Solution:
+
+    def generateInorder(self, root, arr):
+        if root is None:
+            return
+
+        self.generateInorder(root.left, arr)
         arr.append(root.data)
-        getInorder(root.right,arr)
+        self.generateInorder(root.right, arr)
 
-def printPreOrder(root):
+    def generateBST(self, left, right, arr):
 
-    if root:
-        print(root.data,end =" ")
-        printPreOrder(root.left)
-        printPreOrder(root.right)
+        if left > right:
+            return None
 
-def buildTree(left,right,arr):
+        mid = (left + right) // 2
+        currNode = Node(arr[mid])
+        currNode.left = self.generateBST(left, mid - 1, arr)
+        currNode.right = self.generateBST(mid + 1, right, arr)
 
-    if left > right:
-        return None
+        return currNode
 
-    mid = (left + right) // 2
-
-    currNode = Node(arr[mid])
-
-    currNode.left = buildTree(left,mid-1,arr)
-    currNode.right = buildTree(mid+1,right,arr)
-
-    return currNode
-
-def getNewRoot(root):
-
-    arr = []
-    getInorder(root,arr)
-
-    return buildTree(0,len(arr)-1,arr)
-
-
-
-# root = Node(4)
-# root.left = Node(3)
-# root.left.left = Node(2)
-# root.left.left.left = Node(1)
-# root.right = Node(5)
-# root.right.right = Node(6)
-# root.right.right.right = Node(7)
-
-root = Node(10)
-root.left = Node(8)
-root.left.left = Node(7)
-root.left.left.left = Node(6)
-root.left.left.left.left = Node(5)
-
-
-nRoot = getNewRoot(root)
-printPreOrder(nRoot)
-
+    def balanceBST(self, root):
+        arr = []
+        self.generateInorder(root, arr)
+        return self.generateBST(0, len(arr) - 1, arr)
