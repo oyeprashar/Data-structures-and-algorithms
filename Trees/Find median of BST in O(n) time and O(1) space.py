f560a558684e5482dd985bba4c025a971b27804c
Shubham Prashar <sirprashar@gmail.com>
2026-09-26 07:04:42 -0400

    updated code for finding median in BST

--- Trees/Find median of BST in O(n) time and O(1) space.py
+++ Trees/Find median of BST in O(n) time and O(1) space.py
@@ -1,72 +1,53 @@
+"""
+Why always our formula (count[0] + 1) // 2 works?
+
+
+    What the question wants us to return?
+        Even: return value at (n/2).
+        Odd: return value at ((n+1)/2).
+
+    Since we are counting the nodes, (count[0] + 1) // 2 just lands on the correct count!
+"""
+
+
+
+
 class Node:
-    def __init__(self,data):
-        self.data = data
-        self.left = None 
-        self.right = None 
+    def __init__(self, val):
+        self.data = val
+        self.left = None
+        self.right = None
 
-def countNodes(root,count):
 
-    if root != None:
+class Solution:
 
-        countNodes(root.left,count)
+    def getTotalNode(self, root, count):
+
+        if root is None:
+            return
+
+        self.getTotalNode(root.left, count)
         count[0] += 1
-        countNodes(root.right,count)
+        self.getTotalNode(root.right, count)
 
-def findMiddle2(root,currCount,targetCount,mids):
+    def getMiddleTwoNodes(self, root, targetcount, currCount, savedNodes):
 
-    if root != None:
+        if root is None:
+            return None
 
-        findMiddle2(root.left,currCount,targetCount,mids)
+        self.getMiddleTwoNodes(root.left, targetcount, currCount, savedNodes)
 
         currCount[0] += 1
 
-        if currCount[0] == targetCount:
-            mids[0] = root.data
-        
-        elif currCount[0] == targetCount + 1:
-            mids[1] = root.data 
+        if currCount[0] == targetcount:
+            savedNodes[0] = root.data
 
-        findMiddle2(root.right,currCount,targetCount,mids)
+        self.getMiddleTwoNodes(root.right, targetcount, currCount, savedNodes)
 
+    def findMedian(self, root):
 
-def findMedian(root):
-
-    totalCount = [0]
-
-    countNodes(root,totalCount)
-
-    totalCount = totalCount[0]
-
-    targetCount = totalCount // 2
-
-    mids = [None,None]
-    currCount = [0]
-
-    findMiddle2(root,currCount,targetCount,mids)
-
-    if totalCount % 2 == 0:
-        return (mids[0]+mids[1]) / 2
-    
-    else:
-        return mids[1]
-
-root = Node(20)
-root.left = Node(8)
-root.left.left = Node(4)
-root.left.right = Node(12)
-root.left.right.left = Node(10)
-root.left.right.right = Node(14)
-root.right = Node(22)
-
-print(findMedian(root))
-
-
-
-        
-
-
-
-
-
-
-
+        count = [0]
+        self.getTotalNode(root, count)
+        savedNodes = [None]
+        self.getMiddleTwoNodes(root, (count[0] + 1) // 2, [0], savedNodes)
+        return savedNodes[0]
